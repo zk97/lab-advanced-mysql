6ef7301bb389b1cead59a9eeb13c6995465da8bc
Zahid Sanchez <10248069+zk97@users.noreply.github.com>
2020-05-03 13:38:18 -0500

    solved lab

---
+++ your-code/solutions.sql
@@ -0,0 +1,54 @@
+USE publications;
+
+-- CHALLENGE 1
+CREATE TEMPORARY TABLE royalties 
+SELECT titles.title_id AS TitleID, titleauthor.au_id AS AuthorID, 
+titles.price*sales.qty*(titles.royalty/100)*(titleauthor.royaltyper/100) AS Royalty
+FROM titleauthor 
+JOIN titles ON titles.title_id=titleauthor.title_id
+JOIN sales ON titles.title_id=sales.title_id;
+
+CREATE TEMPORARY TABLE royal_au_tit
+SELECT TitleID, AuthorID, 
+SUM(Royalty) AS Royalties
+FROM royalties
+GROUP BY royalties.TitleID, royalties.AuthorID;
+
+SELECT AuthorID, titles.advance*(titleauthor.royaltyper/100)+Royalties AS Profit
+FROM royal_au_tit
+JOIN titles ON royal_au_tit.TitleID=titles.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id
+GROUP BY royal_au_tit.AuthorID
+ORDER BY Profit DESC
+LIMIT 3;
+
+-- CHALLENGE 2
+SELECT AuthorID, titles.advance*(titleauthor.royaltyper/100)+Royalties AS Profit
+FROM (SELECT TitleID, AuthorID, 
+        SUM(Royalty) AS Royalties
+        FROM (SELECT titles.title_id AS TitleID, titleauthor.au_id AS AuthorID, 
+                titles.price*sales.qty*(titles.royalty/100)*(titleauthor.royaltyper/100) AS Royalty
+                FROM titleauthor 
+                JOIN titles ON titles.title_id=titleauthor.title_id
+                JOIN sales ON titles.title_id=sales.title_id) AS royalties
+        GROUP BY royalties.TitleID, royalties.AuthorID) AS royal_au_tit
+        JOIN titles ON royal_au_tit.TitleID=titles.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id
+GROUP BY royal_au_tit.AuthorID
+ORDER BY Profit DESC
+LIMIT 3;
+
+-- CHALLENGE 3
+CREATE TABLE most_profiting_authors SELECT AuthorID AS 'au_id', titles.advance*(titleauthor.royaltyper/100)+Royalties AS 'profits'
+FROM (  SELECT TitleID, AuthorID, 
+        SUM(Royalty) AS Royalties
+        FROM (SELECT titles.title_id AS TitleID, titleauthor.au_id AS AuthorID, 
+                titles.price*sales.qty*(titles.royalty/100)*(titleauthor.royaltyper/100) AS Royalty
+                FROM titleauthor 
+                JOIN titles ON titles.title_id=titleauthor.title_id
+                JOIN sales ON titles.title_id=sales.title_id) AS royalties
+        GROUP BY royalties.TitleID, royalties.AuthorID) AS royal_au_tit
+        JOIN titles ON royal_au_tit.TitleID=titles.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id
+GROUP BY royal_au_tit.AuthorID
+ORDER BY profits DESC;
