180711295f8df9fb64a848883b1d898b3d8ed58b
Zahid Sanchez <10248069+zk97@users.noreply.github.com>
2021-05-17 22:16:50 -0500

    [UPDATE] - Solutions aggregation

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -14,16 +14,16 @@ SUM(Royalty) AS Royalties
 FROM royalties
 GROUP BY royalties.TitleID, royalties.AuthorID;
 
-SELECT AuthorID, titles.advance*(titleauthor.royaltyper/100)+Royalties AS Profit
+SELECT AuthorID, SUM(titles.advance*(titleauthor.royaltyper/100)+Royalties) AS Profit
 FROM royal_au_tit
 JOIN titles ON royal_au_tit.TitleID=titles.title_id
-JOIN titleauthor ON titles.title_id=titleauthor.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id AND royal_au_tit.AuthorID = titleauthor.au_id
 GROUP BY royal_au_tit.AuthorID
 ORDER BY Profit DESC
 LIMIT 3;
 
 -- CHALLENGE 2
-SELECT AuthorID, titles.advance*(titleauthor.royaltyper/100)+Royalties AS Profit
+SELECT AuthorID, SUM(titles.advance*(titleauthor.royaltyper/100)+Royalties) AS Profit
 FROM (SELECT TitleID, AuthorID, 
         SUM(Royalty) AS Royalties
         FROM (SELECT titles.title_id AS TitleID, titleauthor.au_id AS AuthorID, 
@@ -33,13 +33,13 @@ FROM (SELECT TitleID, AuthorID,
                 JOIN sales ON titles.title_id=sales.title_id) AS royalties
         GROUP BY royalties.TitleID, royalties.AuthorID) AS royal_au_tit
         JOIN titles ON royal_au_tit.TitleID=titles.title_id
-JOIN titleauthor ON titles.title_id=titleauthor.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id AND royal_au_tit.AuthorID = titleauthor.au_id
 GROUP BY royal_au_tit.AuthorID
 ORDER BY Profit DESC
 LIMIT 3;
 
 -- CHALLENGE 3
-CREATE TABLE most_profiting_authors SELECT AuthorID AS 'au_id', titles.advance*(titleauthor.royaltyper/100)+Royalties AS 'profits'
+CREATE TABLE most_profiting_authors SELECT AuthorID AS 'au_id', SUM(titles.advance*(titleauthor.royaltyper/100)+Royalties) AS 'profits'
 FROM (  SELECT TitleID, AuthorID, 
         SUM(Royalty) AS Royalties
         FROM (SELECT titles.title_id AS TitleID, titleauthor.au_id AS AuthorID, 
@@ -49,6 +49,6 @@ FROM (  SELECT TitleID, AuthorID,
                 JOIN sales ON titles.title_id=sales.title_id) AS royalties
         GROUP BY royalties.TitleID, royalties.AuthorID) AS royal_au_tit
         JOIN titles ON royal_au_tit.TitleID=titles.title_id
-JOIN titleauthor ON titles.title_id=titleauthor.title_id
+JOIN titleauthor ON titles.title_id=titleauthor.title_id AND royal_au_tit.AuthorID = titleauthor.au_id
 GROUP BY royal_au_tit.AuthorID
 ORDER BY profits DESC;
